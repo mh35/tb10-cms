19b46051c0ceb96af96ae159d24d100b70ac6a4c
mh35 <mh35jp@gmail.com>
2021-01-04 20:28:38 +0900

    Template implementation

--- main.rb
+++ main.rb
@@ -1,6 +1,46 @@
 #!/usr/bin/env ruby
 #coding: utf-8
 
-File.open('dst/out.txt', 'w') do |fp|
-  fp.puts 'Hello!'
+require 'csv'
+require 'erb'
+require 'fileutils'
+
+class TemplateOutput
+  def initialize(template='default.html.erb')
+    tmpl_file = File.join('src/templates', template)
+    File.open(tmpl_file, 'r') do |file|
+      @template = ERB.new(file.read, trim_mode: '-')
+    end
+    @template.filename = tmpl_file
+  end
+  def output(input, output, title)
+    @title = title
+    File.open(input, 'r') do |file|
+      @content = file.read
+    end
+    File.open(output, 'w') do |file|
+      file.write @template.result(binding)
+    end
+  end
+end
+
+FileUtils.cp_r('src/assets', 'dst/assets')
+FileUtils.rm('dst/.keep')
+CSV.open('src/pages/pages.csv', 'r', headers: true) do |csv|
+  csv.each do |row|
+    dname = File.dirname(row['output'])
+    fname = File.basename(row['output'])
+    if dname == '.'
+      out_dname = 'dst'
+    else
+      out_dname = File.join('dst', dname)
+      FileUtils.mkdir_p(out_dname) unless File.exists?(
+        out_dname)
+    end
+    out_fname = File.join(out_dname, fname)
+    in_fname = File.join('src/pages', row['source'])
+    title = row['title']
+    out = TemplateOutput.new
+    out.output(in_fname, out_fname, title)
+  end
 end
